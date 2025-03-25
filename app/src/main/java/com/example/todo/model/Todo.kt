5397c7f7e8c5564429c8d79d6d40db508233914b
SisiYin <t3chyi00@students.oamk.fi>
2025-03-25 20:22:09 +0200

    Profile Ready

--- app/src/main/java/com/example/todo/model/Todo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.todo.model
-
-data class Todo(
-    //@SerializedName("userId") var userId: Int,  // API 返回字段
-    var userId: Int,
-    var id: Int,
-    var title: String,
-    var completed: Boolean
-)
