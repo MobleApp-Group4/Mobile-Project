08bd54c59b2b484a7b666d12ebb6cff97efa5ef7
KeZhang0816 <961351920@qq.com>
2025-04-21 21:09:56 +0300

    1

--- app/src/main/java/com/example/todo/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/todo/ui/screens/ErrorScreen.kt
@@ -3,7 +3,7 @@ package com.example.todo.ui.screens
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-@Composable
+@Composable//
 fun ErrorScreen() {
     Text("Error retrieving data from API.")
 }
